7d1245d984c669bfc7edfeaed601997eaa2de6ba
GGusm4o <guilhermegusmao0811@gmail.com>
2025-07-13 15:59:47 -0400

    Melhorando o code

--- Projetos/OlaMundo/OlaMundo.c
+++ Projetos/OlaMundo/OlaMundo.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main(){
+    //Sintaxe pradrão printf
     printf("Olá, Mundo!\n");
+    //printf("texto com formatação", variavel1, variavel2, ...);
+    //printf("Olá, Mundo!\n");
    
 }
