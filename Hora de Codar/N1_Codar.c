ea5b8f0093e00468ba08452730a0c6e793b6a59e
GGusm4o <guilhermegusmao0811@gmail.com>
2025-07-13 18:27:44 -0400

    Hora de codar
    Sistema de Registro de aluno

---
+++ Hora de Codar/N1_Codar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int idade, matricula;
+    float altura;
+    char nome[50];
+
+    printf("Digite seu nome \n");
+    scanf("%s", nome);
+    printf("Digite sua matricula \n");
+    scanf("%d", &matricula);
+    printf("Digite sua idade \n");
+    scanf("%d", &idade);
+    printf("Digite sua altura \n");
+    scanf("%f", &altura);
+
+    printf("Nome do aluno: %s\ - matricula: %d\n", nome, matricula);
+    printf("idade do aluno: %d\n", idade);
+    printf("Altura: %.2f\n", altura);
+
+    return 0;   
+}
