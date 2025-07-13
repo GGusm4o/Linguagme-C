3656af9fc410b0dd801e240888521e8313cfa55d
GGusm4o <guilhermegusmao0811@gmail.com>
2025-07-13 16:11:19 -0400

    Ajuste no codigo

--- Projetos/EntradaSaídaDados/Entrada.c
+++ Projetos/EntradaSaídaDados/Entrada.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 int main() {
-    int idade = 25;
+
+    int idade = 17;
     float altura = 1.75;
-    double saldoBancario = 12345.67;
-    char inicial = 'A';
-    char nome[20] = "Bruno";
+    char opcao = 'G';
+    char nome[20] = "Guilherme";
  
-    printf("Idade: %d anos\n", idade);
-    printf("Altura: %.2f metros\n", altura);
-    printf("Saldo Bancário: %.2f reais\n", saldoBancario);
-    printf("Inicial do Nome: %c\n", inicial);
-    printf("Nome: %s\n", nome);
+    printf("Idade: %d é %d\n", nome, idade);
+    printf("A altura é: %e\n", altura);
+    printf("A opção é : %c\n", opcao);
  
     return 0;
 
-    /*
+/*
 printf("%formatol formato2 Sformato3", variavell, variavel2, variavel3)
 
-No: Imprime un inteiro no formato decimal.
-
-%1: Equivalente a %0.
+%d: Imprime un número inteiro decimal.
+%i: Equivalente a %d.
 %f: Imprime un número de ponto flutuante no formato padrão.
-%x: Imprime un número inteiro no formato hexadecimal.
 %e: Imprime un número de ponto flutuante na notação cientifica.
 %c: Imprima un único caractere
-%a: Imprime una cadeia (string) de caracteres.
+%s: Imprime uma cadeia (string) de caracteres.
 */
+
 }
