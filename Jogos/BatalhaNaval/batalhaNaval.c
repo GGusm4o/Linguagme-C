e9e7cf9da914fdf9f84a67a019ffa5e5f719f7f4
GGusm4o <guilhermegusmao0811@gmail.com>
2025-08-16 20:58:07 -0400

    Conclusão de nível aventureiro
    Adição de barcos na diagonal principal e secundaria usando loops com matrizes.

--- Jogos/BatalhaNaval/batalhaNaval.c
+++ Jogos/BatalhaNaval/batalhaNaval.c
@@ -11,36 +11,40 @@ int main() {
     char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
     int tabuleiro[LINHAS][COLUNAS];
 
-    for(int i = 0; i < LINHAS; i++)
-    {
-        for(int j = 0; j < COLUNAS; j++)
-        {
+    for(int i = 0; i < LINHAS; i++){
+        for(int j = 0; j < COLUNAS; j++){
             tabuleiro[i][j] = 0;
         }
     }
     // Posiciona um navio na diagonal principal (tamanho 3)
     // Linha 3 (índice 2): colunas C, D, E
-    for (int j = 2; j <= 4; j++) {
-        tabuleiro[2][j] = 3;
+    // Posiciona um navio na diagonal principal e secundária (tamanho 3)
+    //Diagonal principal
+    for (int i = 2; i <= 4; i++) {
+        tabuleiro[i][i] = 3;
     }
-
-    // Linha 7, 8 e 9 (índices 6, 7, 8): coluna I (índice 8)
-    for (int i = 6; i <= 8; i++) {
+    //Diagonal secundaria
+    for (int j = 2; j <= 4; j++) {
+        tabuleiro[j][9-j] = 3;
+    }
+    // Posiciona um navio na horizontal e vertical (tamanho 3)
+    //Horizontal
+    for (int j = 2; j <= 4; j++) {
+        tabuleiro[0][j] = 3;
+    }
+    //Vertical
+    for (int i = 6; i <= 8; i++){
         tabuleiro[i][8] = 3;
     }
     printf("TABULEIRO BATALHA NAVAL:\n");
     printf("   ");
-    for(int j = 0; j < COLUNAS; j++)
-    {
+    for(int j = 0; j < COLUNAS; j++){
         printf("%c ", linha[j]);
     }
     printf("\n");
-    
-    for(int i = 0; i < LINHAS; i++)
-    {
+    for(int i = 0; i < LINHAS; i++){
         printf("%2d ", i+1); // Números de 1 a 10 na vertical
-        for(int j = 0; j < COLUNAS; j++)
-        {
+        for(int j = 0; j < COLUNAS; j++){
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
